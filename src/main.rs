345a387c0062f7a038cc74f3c619c9d1050f0928
Erika Marttinen <erika.marttinen@aalto.fi>
2025-11-21 17:17:35 +0200

    Add symbolic variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use symbolica::{atom::Atom, symbol};
+
+fn make_symbolic_variable(v: &str, exp: usize, z: usize) -> Atom {
+    Atom::from(symbol!(&format!("{v}_p{exp}_{z}")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let var = make_symbolic_variable("v", 3, 2);
+
+    println!("{var:?}");
 }

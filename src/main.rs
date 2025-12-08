b0d4873939f0cdbadedc746c7f65f08ab34dd859
Erika Marttinen <erika.marttinen@aalto.fi>
2025-12-08 10:24:46 +0200

    Find rank of latin squares script

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use itertools::Itertools;
 use tensor_restrictions::{isomorphism::TensorIsomorphisms, symbolica::TensorRankFinder};
 
 fn main() {
-    let dim = 2;
-    let tensor_isomorphisms = TensorIsomorphisms::new_square(dim);
+    let dim = 3;
+    let tensor_isomorphisms = TensorIsomorphisms::new_square_sparse(dim);
+    let sparse_latin_squares = TensorIsomorphisms::get_partial_latin_squares(&tensor_isomorphisms);
 
-    TensorIsomorphisms::print_tensor_isomorphism_classes(&tensor_isomorphisms);
+    println!("--- Sparse Latin square tensors ---");
+    TensorIsomorphisms::print_tensor_isomorphism_classes(&sparse_latin_squares);
 
-    for (&n, classes) in tensor_isomorphisms
+    println!();
+    println!("--- Their ranks ---");
+    for (&n, classes) in sparse_latin_squares
         .get_isomorphism_classes()
         .iter()
         .sorted_by_key(|&(&n, _)| n)
@@ -19,7 +23,7 @@ fn main() {
         println!("nonzero elements: {n}");
         for (i, class) in classes.iter().enumerate() {
             if let Some(representative) = class.first() {
-                let rank = TensorRankFinder::find_tensor_rank(representative, Some(3));
+                let rank = TensorRankFinder::find_tensor_rank(representative, None);
                 println!("\tclass {}: rank <= {}", i + 1, rank);
                 println!("\t\t{:?}", representative);
             }
